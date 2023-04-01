f9db877fcd99f721dcdd602e689086cdb9ffc7b9
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-31 22:17:32 -0400

    homework: funcion to add toys was created

--- toys/addToy.js
+++ toys/addToy.js
@@ -2,14 +2,30 @@ import { client, toysCollection } from "../mongoConnect.js";
 
 client.connect();
 const addToy = async () => {
-  const myToy = {
+  const myToy = [{
     name: "Optimus Prime",
     type: "Transformer",
     cool: true,
     price: 40,
     description: "Leader Autobot, real cool blue semi truck, with sword",
-  };
-  const addedToy = await toysCollection.insertOne(myToy);
+  },
+  {
+    name: "Teddy Bear",
+    type: "Stuffed animal",
+    cool: false,
+    price: 20,
+    description: "YOUR BEarST FRIEND"
+  },
+  {
+    name: "Barbie Computer Engineer",
+    type: "Barbie collecion",
+    cool: true,
+    price: 50,
+    description: "Really cool barbie computer engineer"
+
+  }
+];
+  const addedToy = await toysCollection.insertMany(myToy);
   console.log(addedToy);
 };
 
