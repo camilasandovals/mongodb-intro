a583330a513996626b0e37289493137de58f8734
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-03-31 20:29:29 -0400

    edit function created

--- fruits/editFruits.js
+++ fruits/editFruits.js
@@ -4,8 +4,8 @@ import { client, fruitsCollection } from "../mongoConnect.js";
 const editFruit = async () => {
     //collection.findOne({name: 'Pear'})
     const updatedFruit = await fruitsCollection.findOneAndUpdate(
-      { name: "Pear" },
-      { $set: { name: "Apple", special: true } }
+      { name: "Banana" },
+      { $set: { name: "Orange", special: true } }
     );
     console.log(updatedFruit);
   };
